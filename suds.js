fc5543c067c634ac4007a599432bf4abe63fa7d6
Daniel Thompson <danl_thompson@hotmail.com>
2025-02-23 17:38:42 -0500

    gitignore

--- suds.js
+++
@@ -1,6 +0,0 @@
-const removeIsBack = (str) => str.replace(/\bis back!?(\s|$)/gi, "").trim();
-
-console.log(removeIsBack("The event IS BACK! Don't miss it."));      // Output: "The event Don't miss it."
-console.log(removeIsBack("He is back! and better than ever."));      // Output: "He and better than ever."
-console.log(removeIsBack("Your favorite show is back!"));            // Output: "Your favorite show"
-
